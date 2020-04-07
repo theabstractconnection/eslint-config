2081bc3f792432fbe2b0e138907685c430bc427a
Manuel Wegria <abstract.s33d@gmail.com>
2020-04-07 13:27:50 +0200

    style: lint

--- react.js
+++ react.js
@@ -1,51 +1,51 @@
 module.exports = {
-  extends: ["react-app", "plugin:prettier/recommended"],
-  plugins: ["simple-import-sort"],
+  extends: ['react-app', 'eslint:recommended', 'plugin:prettier/recommended'],
+  plugins: ['simple-import-sort'],
   rules: {
-    "simple-import-sort/sort": "error",
-    "react/boolean-prop-naming": [
-      "warn",
-      { validateNested: true, rule: "^(is|has)[A-Z]([A-Za-z0-9]?)+" },
+    'simple-import-sort/sort': 'error',
+    'react/boolean-prop-naming': [
+      'warn',
+      { validateNested: true, rule: '^(is|has)[A-Z]([A-Za-z0-9]?)+' },
     ], // Enforce consistent boolean naming
-    "react/destructuring-assignment": ["warn", "always"],
-    "react/function-component-definition": [
-      "warn",
+    'react/destructuring-assignment': ['warn', 'always'],
+    'react/function-component-definition': [
+      'warn',
       {
-        namedComponents: "arrow-function",
-        unnamedComponents: "function-expression",
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'function-expression',
       },
     ],
-    "react/no-adjacent-inline-elements": "warn",
-    "react/no-array-index-key": "warn",
-    "react/no-danger": "error",
-    "react/no-deprecated": "error",
-    "react/no-this-in-sfc": "warn",
-    "react/prefer-stateless-function": "warn",
-    "react/forbid-prop-types": [
-      "warn",
+    'react/no-adjacent-inline-elements': 'warn',
+    'react/no-array-index-key': 'warn',
+    'react/no-danger': 'error',
+    'react/no-deprecated': 'error',
+    'react/no-this-in-sfc': 'warn',
+    'react/prefer-stateless-function': 'warn',
+    'react/forbid-prop-types': [
+      'warn',
       {
-        forbid: ["any", "array", "object"],
+        forbid: ['any', 'array', 'object'],
         checkContextTypes: true,
         checkChildContextTypes: true,
       },
     ],
-    "react/jsx-handler-names": [
-      "warn",
+    'react/jsx-handler-names': [
+      'warn',
       {
-        eventHandlerPrefix: "handle",
-        eventHandlerPropPrefix: "on",
+        eventHandlerPrefix: 'handle',
+        eventHandlerPropPrefix: 'on',
       },
     ],
-    "react/prop-types": ["warn", { ignore: ["children"] }],
-    "react/jsx-pascal-case": [
-      "error",
+    'react/prop-types': ['warn', { ignore: ['children'] }],
+    'react/jsx-pascal-case': [
+      'error',
       {
         allowAllCaps: true,
         ignore: [],
       },
     ],
-    "react/sort-prop-types": [
-      "warn",
+    'react/sort-prop-types': [
+      'warn',
       {
         ignoreCase: true,
         callbacksLast: false,
@@ -53,8 +53,8 @@ module.exports = {
         sortShapeProp: true,
       },
     ],
-    "react/jsx-sort-props": [
-      "warn",
+    'react/jsx-sort-props': [
+      'warn',
       {
         ignoreCase: true,
         callbacksLast: false,
@@ -64,17 +64,17 @@ module.exports = {
         reservedFirst: true,
       },
     ],
-    "react/jsx-sort-default-props": [
-      "warn",
+    'react/jsx-sort-default-props': [
+      'warn',
       {
         ignoreCase: true,
       },
     ],
-    "react/no-did-update-set-state": "error",
-    "react/no-will-update-set-state": "error",
-    "react/no-direct-mutation-state": "off",
-    "react/no-set-state": "off",
-    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+    'react/no-did-update-set-state': 'error',
+    'react/no-will-update-set-state': 'error',
+    'react/no-direct-mutation-state': 'off',
+    'react/no-set-state': 'off',
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
   },
-};
+}
--- vue.js
+++ vue.js
@@ -5,18 +5,19 @@ module.exports = {
     browser: true,
   },
   extends: [
-    "plugin:vue/recommended",
-    "eslint:recommended",
-    "prettier/vue",
-    "plugin:prettier/recommended",
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'prettier/vue',
+    'plugin:prettier/recommended',
   ],
   rules: {
-    "vue/component-name-in-template-casing": ["error", "PascalCase"],
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'vue/html-quotes': ['error', 'double' | 'single', { avoidEscape: true }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   parserOptions: {
-    sourceType: "module",
-    parser: "babel-eslint",
+    sourceType: 'module',
+    parser: 'babel-eslint',
   },
-};
+}
