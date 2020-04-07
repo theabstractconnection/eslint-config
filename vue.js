a857b6cec0dd85acdd2d50852bab8cf02538dae1
Manuel Wegria <abstract.s33d@gmail.com>
2020-04-07 13:51:41 +0200

    fix: vue/html-quotes

--- vue.js
+++ vue.js
@@ -12,7 +12,7 @@ module.exports = {
   ],
   rules: {
     'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-    'vue/html-quotes': ['error', 'double' | 'single', { avoidEscape: true }],
+    'vue/html-quotes': ['error', 'double', { avoidEscape: true }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
